f20ed2da57cae12a125fad84d4be8218dc2bb31d
KANOKNAPApw <684244103@mail.pbru.ac.th>
2025-10-30 11:27:53 +0700

    Update Ex1.6.2.cpp

--- Ex1.6.2.cpp
+++ Ex1.6.2.cpp
@@ -1,33 +1,45 @@
 #include <bits/stdc++.h>
-#include <cfloat>
+#include <iostream>
 using namespace std;
 
 int main() {
     float num1, num2, result;
     char op;
 
-    cout << "Enter an operator (+, -, *, /): ";
+    cout << "Enter first operand: ";
+    cin >> num1;
+
+    cout << "Enter second operand: ";
+    cin >> num2;
+
+    cout << "Enter operator (+, -, *, /): ";
     cin >> op;
 
-
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
-
-    if (op == '+')
+    if (op == '+') {
         result = num1 + num2;
-    else if (op == '-')
+        cout << num1 << " + " << num2 << " = " << result;
+    } 
+    else if (op == '-') {
         result = num1 - num2;
-    else if (op == '*')
+        cout << num1 << " - " << num2 << " = " << result;
+    } 
+    else if (op == '*') {
         result = num1 * num2;
-    else if (op == '/')
-        result = num1 / num2;
+        cout << num1 << " * " << num2 << " = " << result;
+    } 
+    else if (op == '/') {
+        if (num2 != 0) {
+            result = num1 / num2;
+            cout << num1 << " / " << num2 << " = " << result;
+        } else {
+            cout << "Error! Division by zero.";
+            return 1;
+        }
+    } 
     else {
-        cout << "Error! Operator is not correct";
-        result = -DBL_MAX;
+        cout << "Error! Operator is not correct.";
+        return 1;
     }
-
-    if (result != -DBL_MAX)
-        cout << "Result: " << result;
 
     return 0;
 }
